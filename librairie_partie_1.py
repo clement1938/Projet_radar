d400f2c2647b6c51a58343e1a09804644e112796
ElChipito <mateogaudin65@gmail.com>
2024-11-27 16:51:59 +0100

    les codes de la partie 1

---
+++ librairie_partie_1.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 27 16:47:17 2024
+
+@author: mateo
+"""
+
+#Library Partie 1
---
+++ main_partie_1.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 27 16:41:22 2024
+
+@author: mateo
+"""
+
+# MAIN PARTIE 1
